77420a4d3cbccc9fb09735408774debccff29aca
Armin Naseri <if23b066@technikum-wien.at>
2024-04-17 18:45:55 +0200

    db type sht

--- db/dataHandler.php
+++ db/dataHandler.php
@@ -17,15 +17,13 @@ class DataHandler
 
         $demodata = [];
 
+
         while ($row = mysqli_fetch_assoc($result)) {
-            $demodata[] = new Appointment($row['appID'], $row['name'], 
-            $row['title'], $row['date'], $row['comment'], $row['starttime'], $row['endtime']);
+            $demodata[] = new Appointment($row['appID'], $row['name'], $row['title'], 
+            $row['date'], $row['comment'], $row['starttime'], $row['endtime']);
         }
 
-        $demodata = [
-            [new Appointment(1, "Raphi", "Termin", "01.01.2025", "nigga", "08:00", "17:00")],
-            [new Appointment(2, "No", "ABC", "01.01.1000", "kurd", "06:15", "12:00")]
-        ];
+
         return $demodata;
     }
 }
